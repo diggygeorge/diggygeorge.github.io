850cb462a6464a6ed245ca6790f5d934e3a3bafb
Daniel George <diggyzar@bu.edu>
2025-01-23 12:35:59 -0500

    temporarily removed keyframes for now

--- script.js
+++ script.js
@@ -10,15 +10,11 @@ function input(event) {
     {
         document.getElementById("projects").style.display = "none";
         document.getElementById("about").style.display = "block";
-        document.getElementById("projects").classList.toggle("fadeout");
-        document.getElementById("about").classList.toggle("fadein");
     }
     else if (button === "Projects")
     {
         document.getElementById("projects").style.display = "block";
         document.getElementById("about").style.display = "none";
-        document.getElementById("projects").classList.toggle("fadein");
-        document.getElementById("about").classList.toggle("fadeout");
     }
 }
 
