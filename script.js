20a1ac0c76aa928a09a46640c235608d70f47ac2
Daniel George <diggyzar@bu.edu>
2025-01-30 12:37:34 -0500

    added background toggle, about section

--- script.js
+++ script.js
@@ -2,22 +2,48 @@ function input(event) {
     const clickedButton = event.target;
     button = clickedButton.innerText;
 
-    if (button === "Daniel George")
-    {
+    // Check which button is clicked and display the correct section
+    if (button === "Daniel George") {
         document.getElementById("projects").style.display = "none";
         document.getElementById("about").style.display = "block";
-    }
-    else if (button === "Projects")
-    {
+        document.getElementById("about").classList.add("fadein");
+    } else if (button === "Projects") {
         document.getElementById("projects").style.display = "flex";
         document.getElementById("about").style.display = "none";
+        document.getElementById("projects").classList.add("fadein");
     }
 }
 
-document.getElementById("about").classList.toggle("fadein");
-document.getElementById("projects").classList.toggle("fadein");
+// Fade logic for "about" and "projects"
+document.getElementById("about").classList.add("fadein");
+document.getElementById("projects").classList.add("fadein");
 
 const buttons = document.querySelectorAll(".button");
 buttons.forEach(button => {
     button.addEventListener("click", input);
+})
+
+var count = 0;
+document.getElementById("special").addEventListener("click", function () {
+    count++;
+    console.log(count);
+    if (count % 3 === 0)
+    {
+        document.documentElement.classList.remove("night");
+        document.documentElement.classList.add("morning");
+        document.getElementById("about-description").style.color = "#000000";
+        document.getElementById("proj-description").style.color = "#000000";
+    }
+    if (count % 3 === 1)
+    {
+        document.documentElement.classList.remove("morning");
+        document.documentElement.classList.add("evening");
+        document.getElementById("about-description").style.color = "#f9f9f9";
+        document.getElementById("proj-description").style.color = "#f9f9f9";
+    }
+    if (count % 3 === 2)
+    {
+        document.documentElement.classList.remove("evening");
+        document.documentElement.classList.add("night");
+    }
 })
